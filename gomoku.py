cfb8b39f4e75212c3097adac7ae49fcf8219fde0
Brunofell <brunofelimarti@gmail.com>
2026-05-08 23:07:47 -0300

    opa

---
+++ gomoku.py
@@ -0,0 +1,14 @@
+import time
+import math
+import random
+
+# Constantes
+SIZE = 9
+EMPTY = 0
+BLACK = 1
+WHITE = 2
+COLS = "ABCDEFGHI"
+SYMBOLS = {EMPTY: ".", BLACK: "X", WHITE: "O"} # Depos da pra gente tentar colcoar algo pra colorir no console de preto e branco, fá ouvi falar que da pra fazer algo assim...
+
+class Board: 
+     
